7e40b2bb6e953bdf4434c76644585b59b9d91051
Andor Polgar <hngrhorace@gmail.com>
2016-06-12 18:27:02 +0200

    refactoring

--- src/controlChange.js
+++ src/controlChange.js
@@ -1,58 +1,53 @@
-export default {
-	/*
-	 * Control change
-	 */
-	BANK_SELECT                      : 0x00,
-	MODULATION_WHEEL                 : 0x01,
-	BREATH_CONTROLLER                : 0x02,
-	FOOT_CONTROLLER                  : 0x04,
-	PORTAMENTO_TIME                  : 0x05,
-	DATA_ENTRY_MSB                   : 0x06,
-	CHANNEL_VOLUME                   : 0x07,
-	BALANCE                          : 0x08,
-	PAN                              : 0x0a,
-	EXPRESSION_CONTROLLER            : 0x0b,
-	EFFECT_CONTROL_1                 : 0x0c,
-	EFFECT_CONTROL_2                 : 0x0d,
-	GENERAL_PURPOSE_CONTROLLER_1     : 0x10,
-	GENERAL_PURPOSE_CONTROLLER_2     : 0x11,
-	GENERAL_PURPOSE_CONTROLLER_3     : 0x12,
-	GENERAL_PURPOSE_CONTROLLER_4     : 0x13,
-	BANK_SELECT_LSB                  : 0x20,
-	MODULATION_WHEEL_LSB             : 0x21,
-	BREATH_CONTROLLER_LSB            : 0x22,
-	FOOT_CONTROLLER_LSB              : 0x24,
-	PORTAMENTO_TIME_LSB              : 0x25,
-	DATA_ENTRY_LSB                   : 0x26,
-	CHANNEL_VOLUME_LSB               : 0x27,
-	BALANCE_LSB                      : 0x28,
-	PAN_LSB                          : 0x2a,
-	EXPRESSION_CONTROLLER_LSB        : 0x2b,
-	EFFECT_CONTROL_1_LSB             : 0x2c,
-	EFFECT_CONTROL_2_LSB             : 0x2d,
-	GENERAL_PURPOSE_CONTROLLER_1_LSB : 0x30,
-	GENERAL_PURPOSE_CONTROLLER_2_LSB : 0x31,
-	GENERAL_PURPOSE_CONTROLLER_3_LSB : 0x32,
-	GENERAL_PURPOSE_CONTROLLER_4_LSB : 0x33,
-	PORTAMENTO_ON_OFF                : 0x41,
-	SOSTENUTO_ON_OFF                 : 0x42,
-	SOFT_PEDAL_ON_OFF                : 0x43,
-	LEGATO_FOOTSWITCH                : 0x44,
-	HOLD                             : 0x45,
-	SOUND_CONTROLLER_1               : 0x46,
-	SOUND_CONTROLLER_2               : 0x47,
-	SOUND_CONTROLLER_3               : 0x48,
-	SOUND_CONTROLLER_4               : 0x49,
-	SOUND_CONTROLLER_5               : 0x4a,
-	SOUND_CONTROLLER_6               : 0x4b,
-	SOUND_CONTROLLER_7               : 0x4c,
-	SOUND_CONTROLLER_8               : 0x4d,
-	SOUND_CONTROLLER_9               : 0x4e,
-	SOUND_CONTROLLER_10              : 0x4f,
-	GENERAL_PURPOSE_CONTROLLER_5     : 0x50,
-	GENERAL_PURPOSE_CONTROLLER_6     : 0x51,
-	GENERAL_PURPOSE_CONTROLLER_7     : 0x52,
-	GENERAL_PURPOSE_CONTROLLER_8     : 0x53,
-	PORTAMENTO_CONTROL               : 0x54,
-	HIGH_RESOLUTION_VELOCITY_PREFIX  : 0x58,
-};
+export const BANK_SELECT                      = 0x00;
+export const MODULATION_WHEEL                 = 0x01;
+export const BREATH_CONTROLLER                = 0x02;
+export const FOOT_CONTROLLER                  = 0x04;
+export const PORTAMENTO_TIME                  = 0x05;
+export const DATA_ENTRY_MSB                   = 0x06;
+export const CHANNEL_VOLUME                   = 0x07;
+export const BALANCE                          = 0x08;
+export const PAN                              = 0x0a;
+export const EXPRESSION_CONTROLLER            = 0x0b;
+export const EFFECT_CONTROL_1                 = 0x0c;
+export const EFFECT_CONTROL_2                 = 0x0d;
+export const GENERAL_PURPOSE_CONTROLLER_1     = 0x10;
+export const GENERAL_PURPOSE_CONTROLLER_2     = 0x11;
+export const GENERAL_PURPOSE_CONTROLLER_3     = 0x12;
+export const GENERAL_PURPOSE_CONTROLLER_4     = 0x13;
+export const BANK_SELECT_LSB                  = 0x20;
+export const MODULATION_WHEEL_LSB             = 0x21;
+export const BREATH_CONTROLLER_LSB            = 0x22;
+export const FOOT_CONTROLLER_LSB              = 0x24;
+export const PORTAMENTO_TIME_LSB              = 0x25;
+export const DATA_ENTRY_LSB                   = 0x26;
+export const CHANNEL_VOLUME_LSB               = 0x27;
+export const BALANCE_LSB                      = 0x28;
+export const PAN_LSB                          = 0x2a;
+export const EXPRESSION_CONTROLLER_LSB        = 0x2b;
+export const EFFECT_CONTROL_1_LSB             = 0x2c;
+export const EFFECT_CONTROL_2_LSB             = 0x2d;
+export const GENERAL_PURPOSE_CONTROLLER_1_LSB = 0x30;
+export const GENERAL_PURPOSE_CONTROLLER_2_LSB = 0x31;
+export const GENERAL_PURPOSE_CONTROLLER_3_LSB = 0x32;
+export const GENERAL_PURPOSE_CONTROLLER_4_LSB = 0x33;
+export const PORTAMENTO_ON_OFF                = 0x41;
+export const SOSTENUTO_ON_OFF                 = 0x42;
+export const SOFT_PEDAL_ON_OFF                = 0x43;
+export const LEGATO_FOOTSWITCH                = 0x44;
+export const HOLD                             = 0x45;
+export const SOUND_CONTROLLER_1               = 0x46;
+export const SOUND_CONTROLLER_2               = 0x47;
+export const SOUND_CONTROLLER_3               = 0x48;
+export const SOUND_CONTROLLER_4               = 0x49;
+export const SOUND_CONTROLLER_5               = 0x4a;
+export const SOUND_CONTROLLER_6               = 0x4b;
+export const SOUND_CONTROLLER_7               = 0x4c;
+export const SOUND_CONTROLLER_8               = 0x4d;
+export const SOUND_CONTROLLER_9               = 0x4e;
+export const SOUND_CONTROLLER_10              = 0x4f;
+export const GENERAL_PURPOSE_CONTROLLER_5     = 0x50;
+export const GENERAL_PURPOSE_CONTROLLER_6     = 0x51;
+export const GENERAL_PURPOSE_CONTROLLER_7     = 0x52;
+export const GENERAL_PURPOSE_CONTROLLER_8     = 0x53;
+export const PORTAMENTO_CONTROL               = 0x54;
+export const HIGH_RESOLUTION_VELOCITY_PREFIX  = 0x58;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,144 @@
-import * as Functions from './functions';
 import MidiNotes from './midiNotes';
-import StatusCodes from './statusCodes';
-import ControlChange from './controlChange';
+import * as StatusCodes from './statusCodes';
+import * as ControlChange from './controlChange';
 
-var Midinette = {
+class Midinette {
+	static isMIDIStatus(code) {
+		if (typeof code !== 'number') {
+			return false;
+		}
+
+		return (
+			code >= StatusCodes.NOTE_OFF_CH1 &&
+			code <= StatusCodes.PITCH_WHEEL_CH16
+		);
+	}
+
+	static isMIDIMessage(code) {
+		return (
+			typeof code === 'number' &&
+			Number.isInteger(code) &&
+			code >= 0x000000 &&
+			code <= 0xffffff
+		);
+	}
+
+	static isMIDIByteArray(byteArray) {
+		return (
+			Array.isArray(byteArray) &&
+			byteArray.length === 3 &&
+			Number.isInteger(byteArray[0]) &&
+			Number.isInteger(byteArray[1]) &&
+			Number.isInteger(byteArray[2]) &&
+			byteArray[0] >= 0x00 && byteArray[0] <= 0xff &&
+			byteArray[1] >= 0x00 && byteArray[1] <= 0xff &&
+			byteArray[2] >= 0x00 && byteArray[2] <= 0xff
+		);
+	}
+
+	static intToByteArray(int) {
+		if (Midinette.isMIDIByteArray(int)) {
+			return int;
+		}
+		return [int >> 16, (int >> 8) & 0x00ff,	int & 0x0000ff];
+	}
+
+	static getMIDIStatus(code) {
+		if (Midinette.isMIDIStatus(code)) {
+			return code;
+		}
+		else if (Midinette.isMIDIByteArray(code)) {
+			return code[0];
+		}
+		else if (Midinette.isMIDIMessage(code)) {
+			return getMIDIEvent(Midinette.intToByteArray(code));
+		}
+
+		return 0;
+	}
+
+	static isNoteOn(code) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.NOTE_ON_CH1 &&
+			code <= StatusCodes.NOTE_ON_CH16
+		);
+	}
+
+	static isNoteOff(code) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.NOTE_OFF_CH1 &&
+			code <= StatusCodes.NOTE_OFF_CH16
+		);
+	}
+
+	static isControlChange(code) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.CONTROL_CHANGE_CH1 &&
+			code <= StatusCodes.CONTROL_CHANGE_CH16
+		);
+	}
+
+	static isPitchWheel(code) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.PITCH_WHEEL_CH1 &&
+			code <= StatusCodes.PITCH_WHEEL_CH16
+		);
+	};
+
+	static isPolyphonicAftertouch(data) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.POLYPHONIC_AFTERTOUCH_CH1 &&
+			code <= StatusCodes.POLYPHONIC_AFTERTOUCH_CH16
+		);
+	}
+
+	static isProgramChange(data) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.PROGRAM_CHANGE_CH1 &&
+			code <= StatusCodes.PROGRAM_CHANGE_CH16
+		);
+	}
+
+	static isChannelAftertouch(data) {
+		return (
+			Midinette.isMIDIStatus(code) &&
+			code >= StatusCodes.CHANNEL_AFTERTOUCH_CH1 &&
+			code <= StatusCodes.CHANNEL_AFTERTOUCH_CH16
+		);
+	}
+
+	static constructMIDIMessage(event, channel, data1, data2) {
+		return [(event & 0xf0) + (channel - 1), data1, data2];
+	}
+
+	static noteStringToMIDICode(note) {
+		if (
+			typeof note === 'string' &&
+			typeof Midinette[note] === 'number'
+		) {
+			return Midinette[note];
+		}
+		else if (typeof note === 'number') {
+			return note;
+		}
+		return 0;
+	}
+
+	static getChannelFromStatus(status) {
+		return (status % 0x10) + 1;
+	}
+}
+
+Object.assign(Midinette, {
 	Notes         : MidiNotes,
 	StatusCodes   : StatusCodes,
 	ControlChange : ControlChange
-};
-
-Object.assign(Midinette, Functions);
+});
 
 export default Midinette;
